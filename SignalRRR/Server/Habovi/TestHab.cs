2c072763f3f48bcd42637df47331960bc9bfb414
Dragan Pepic <d.pepic@hotmail.com>
2020-06-23 17:46:39 +0200

    Add project files.

---
+++ SignalRRR/Server/Habovi/TestHab.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRRR.Server.Habovi
+{
+	public class TestHab : Hub
+	{
+		public void Test(string poruka)
+		{
+			Console.WriteLine("Javio se klijent sa : " + poruka);
+			Clients.Caller.SendAsync("ZaServer", poruka.ToUpper());
+		}
+	}
+}
